3c0522e729cbb5de56ee2d4e1a8bf83cfcf376c2
Amateur Programmer <solo2244rt@gmail.com>
2026-08-29 17:32:53 +0700

    Replace pscp with scp for file transfers

--- MinetestCopy.bat
+++ MinetestCopy.bat
@@ -10,10 +10,11 @@ if %errorLevel% NEQ 0 (
     exit /b 1
 )
 
-where pscp >nul 2>&1
+where scp >nul 2>&1
 if %errorLevel% NEQ 0 (
-    echo Не найдена программа pscp.exe.
-    echo Установите PuTTY с сайта https://www.putty.org/ и убедитесь, что pscp доступен в PATH.
+    echo Не найдена программа scp.
+    echo В Windows 10/11 она встроена по умолчанию. Если её нет, включите
+    echo компонент "OpenSSH Client" в Параметры - Приложения - Дополнительные компоненты.
     pause
     exit /b 1
 )
@@ -38,11 +39,11 @@ echo.
 
 set /p SERVER_IP="IP-адрес сервера: "
 set /p SERVER_USER="Логин (обычно root): "
-set /p SERVER_PASS="Пароль: "
 
 echo.
 echo Сейчас будут скачаны ВСЕ файлы проекта с %SERVER_USER%@%SERVER_IP%
 echo в папку C:\minetest
+echo Пароль сервера будет запрошен отдельно для каждой папки.
 echo.
 set /p CONFIRM="Продолжить? (y/n): "
 if /i not "%CONFIRM%"=="y" (
@@ -55,22 +56,22 @@ mkdir C:\minetest 2>nul
 
 echo.
 echo Скачивание /root ...
-pscp -r -pw %SERVER_PASS% %SERVER_USER%@%SERVER_IP%:/root C:\minetest\root
+scp -r %SERVER_USER%@%SERVER_IP%:/root C:\minetest\root
 
 echo Скачивание /opt ...
-pscp -r -pw %SERVER_PASS% %SERVER_USER%@%SERVER_IP%:/opt C:\minetest\opt
+scp -r %SERVER_USER%@%SERVER_IP%:/opt C:\minetest\opt
 
 echo Скачивание /home ...
-pscp -r -pw %SERVER_PASS% %SERVER_USER%@%SERVER_IP%:/home C:\minetest\home
+scp -r %SERVER_USER%@%SERVER_IP%:/home C:\minetest\home
 
 echo Скачивание /etc/minetest ...
-pscp -r -pw %SERVER_PASS% %SERVER_USER%@%SERVER_IP%:/etc/minetest C:\minetest\etc-minetest
+scp -r %SERVER_USER%@%SERVER_IP%:/etc/minetest C:\minetest\etc-minetest
 
 echo Скачивание /etc/systemd/system ...
-pscp -r -pw %SERVER_PASS% %SERVER_USER%@%SERVER_IP%:/etc/systemd/system C:\minetest\etc-systemd
+scp -r %SERVER_USER%@%SERVER_IP%:/etc/systemd/system C:\minetest\etc-systemd
 
 echo Скачивание /var/www ...
-pscp -r -pw %SERVER_PASS% %SERVER_USER%@%SERVER_IP%:/var/www C:\minetest\var-www
+scp -r %SERVER_USER%@%SERVER_IP%:/var/www C:\minetest\var-www
 
 echo.
 echo Готово. Все файлы находятся в папке C:\minetest
